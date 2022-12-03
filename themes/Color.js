942b102129bc219fd1520502a3723a0be0142e19
joysen <joysenr96@gmail.com>
2022-12-03 08:55:12 +0600

    first commit

---
+++ themes/Color.js
@@ -0,0 +1,11 @@
+const Color = {
+  White: "#FFF",
+  black: "#222623",
+  green: "#218a3d",
+  red: "#cf2743",
+  gray: "#2a302f",
+  yellow: "#c78d1a",
+  blue: "#323eed",
+  orange: "#ed6d32",
+};
+export default Color;
---
+++ themes/Typography.js
@@ -0,0 +1,6 @@
+export const Typography = {
+  primary: "Spartan-Regular",
+  primary_bold: "Spartan-Medium",
+  secondary: "Antonio-Medium",
+  secondary_bold: "Antonio-Bold",
+};
